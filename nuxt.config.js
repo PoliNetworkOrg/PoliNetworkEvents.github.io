0c86d96d6bba86e99613c5f2d682d2e65cb5e9d9
steven9463912 <steven9463912@gmail.com>
2022-02-13 12:27:52 +0100

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,98 +1,96 @@
 export default {
 	// Target: https://go.nuxtjs.dev/config-target
-	target: 'static',
-
+	target: "static",
+  
 	// Global page headers: https://go.nuxtjs.dev/config-head
 	head: {
-		title: 'PoliNetwork Events 🥳',
-		htmlAttrs: {
-			lang: 'en'
+	  title: "PoliNetworkEvents",
+	  htmlAttrs: {
+		lang: "en",
+	  },
+	  meta: [
+		{ charset: "utf-8" },
+		{ name: "viewport", content: "width=device-width, initial-scale=1" },
+		{ hid: "description", name: "description", content: "" },
+	  ],
+	  link: [{ rel: "icon", type: "image/x-icon", href: "/img/icon.png" }],
+	  script: [
+		{
+		  src: "/jquery/jquery.min.js",
+		  type: "text/javascript",
 		},
-		meta: [
-			{ charset: 'utf-8' },
-			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
-			{ hid: 'description', name: 'description', content: '' }
-		],
-		link: [
-			{ rel: 'icon', type: 'image/x-icon', href: '/img/icon.png' }
-		],
-		script: [
-			{
-				src: "/jquery/jquery.min.js",
-				type: "text/javascript"
-			},
-			{
-				src: "/js/bootstrap.bundle.min.js",
-				type: "text/javascript"
-			},
-			{
-				src: "/js/custom.js",
-				type: "text/javascript"
-			},
-		]
+		{
+		  src: "/js/bootstrap.bundle.min.js",
+		  type: "text/javascript",
+		},
+		{
+		  src: "/js/custom.js",
+		  type: "text/javascript",
+		},
+	  ],
 	},
-
+  
 	// Global CSS: https://go.nuxtjs.dev/config-css
 	css: [
-		'~/assets/bootstrap/bootstrap.css',
-		'~/assets/bootstrap/style.css',
-		'~/assets/css/style.css'
+	  "~/assets/bootstrap/bootstrap.css",
+	  "~/assets/bootstrap/style.css",
+	  "~/assets/css/style.css",
 	],
-
+  
 	// Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-	plugins: [
-	],
-
+	plugins: [],
+  
 	// Auto import components: https://go.nuxtjs.dev/config-components
 	components: true,
-
+  
 	// Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-	buildModules: [
-	],
-
-	modules: [
-		'@nuxtjs/i18n',
-		'nuxt-i18n-easy',
-	],
-	i18n: {
-		strategy: 'prefix',
-		langDir: 'locales/',
-		locales: [
-			{
-				code: 'en',
-				iso: 'en-US',
-				file: 'en-US.js',
-				isCatchallLocale: true
-			},
-			{
-				code: 'it',
-				iso: 'it-IT',
-				file: 'it-IT.js'
-			}
+	buildModules: [],
+  
+	build: {
+	  babel: {
+		presets({ isServer }) {
+		  const targets = isServer ? { node: "current" } : { ie: 11 };
+		  return [[require.resolve("@babel/preset-env"), { targets }]];
+		},
+		plugins: [
+		  "@babel/syntax-dynamic-import",
+		  "@babel/transform-runtime",
+		  "@babel/transform-async-to-generator",
 		],
-		defaultLocale: 'it',
-		vueI18n: {
-			fallbackLocale: 'it'
-		}
+	  },
+	},
+  
+	modules: ["nuxt-i18n"],
+  
+	i18n: {
+	  locales: [
+		{
+		  code: "en",
+		  name: "English",
+		  file: "en.js",
+		},
+		{
+		  code: "it",
+		  name: "Italiano",
+		  file: "it.js",
+		},
+	  ],
+	  strategy: "prefix",
+	  langDir: "lang/",
+	  defaultLocale: "it",
+	  vueI18n: {
+		fallbackLocale: "it",
+	  },
 	},
 	i18nEasy: {
-		sourceLanguage: 'it',
+	  directories: [
+		// default directories for search
+		"./layouts",
+		"./pages",
+		"./components",
+	  ],
+	  files: ["*.vue", "*.js"], // default files
+	  sourceLanguage: "it", // default source language
 	},
-
-	// Build Configuration: https://go.nuxtjs.dev/config-build
-	build: {
-		babel: {
-			presets({ isServer }) {
-				const targets = isServer ? { node: 'current' } : { ie: 11 }
-				return [
-					[require.resolve("@babel/preset-env"), { targets }]
-				]
-			},
-			plugins: [
-				"@babel/syntax-dynamic-import",
-				"@babel/transform-runtime",
-				"@babel/transform-async-to-generator"
-			]
-		}
-	}
-}
+  };
+  
