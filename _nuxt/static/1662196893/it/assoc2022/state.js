b03e7526af71f84620b91b8be56ad00979c0f244
steven9463912 <steven9463912@users.noreply.github.com>
2022-09-03 10:07:58 +0000

    deploy: cad8cedd06054fef0d68b8fbee35726ea37e793c

--- _nuxt/static/1662196893/it/assoc2022/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1662196893",layout:"default",error:a,serverRendered:true,routePath:"\u002Fit\u002Fassoc2022",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
